578dfee46d76a7c2c4016dda89dcae988ff44dc8
VincentGaligaro <jhangvincent19@gmail.com>
2024-11-09 13:31:30 +0800

    userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Vincent"; 
+let age = 22; 
+let favoriteNumber = 19; 
+let favoriteColor = "Red"; 
+
+console.log(`Full Name: ${fullName}`);
+console.log(`Age: ${age}`);
+console.log(`Favorite Number: ${favoriteNumber}`);
+console.log(`Favorite Color: ${favoriteColor}`);
